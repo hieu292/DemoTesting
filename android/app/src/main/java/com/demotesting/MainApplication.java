1f7df54f02fe3abce712c3e5f2a01e0bddf9f3c3
nhhieu-ibl <hieunh@blockchainlabs.asia>
2018-12-20 16:15:47 +0700

    add braintree packages

--- android/app/src/main/java/com/demotesting/MainApplication.java
+++ android/app/src/main/java/com/demotesting/MainApplication.java
@@ -3,6 +3,7 @@ package com.demotesting;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import tech.bam.RNBraintreeDropIn.RNBraintreeDropInPackage;
 import com.i18n.reactnativei18n.ReactNativeI18n;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNBraintreeDropInPackage(),
             new ReactNativeI18n(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
